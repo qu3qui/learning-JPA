5bb7c55064d306ba04e7c66853095225f14beef7
jbuitrago <jbuitrago@PC-3600-JLGX1.heinsohn.com.co>
2017-10-13 11:44:33 -0500

    Capitulo 8 - Merge y Remove

--- proyectoEjemplo/src/main/java/proyectoEjemplo/tests/EmpleadoTest.java
+++ proyectoEjemplo/src/main/java/proyectoEjemplo/tests/EmpleadoTest.java
@@ -14,13 +14,13 @@ import proyectoEjemplo.entidades.Empleado;
 public class EmpleadoTest {
 	
 	//@PersistenceContext(unitName = "Persistencia") -> Se usa cuando tenemos un contenedor y de ahi se extrae el contexto de persistencia
-	private static EntityManager manager;
+	
 	private static EntityManagerFactory emf = Persistence.createEntityManagerFactory("my-pu");
 	
 	public static void main (String [] args) {
 		
 		//Se crea el gestor de persistencia
-		manager = emf.createEntityManager();
+		EntityManager manager = emf.createEntityManager();
 		
 		crearRegistro();
 		
@@ -31,21 +31,44 @@ public class EmpleadoTest {
 		e.setNombre("Nombre modificado");
 		e.setApellidos("Apellido Modificado");
 		manager.getTransaction().commit();
-		
 		imprimirTodo();
 		//Esta sentencia se utiliza para cerrar el manager y liberar recursos, Importante revisar esto
 		manager.close();
+		
+		//Aca vemos como al cerrar el entitymanager y volver a abrirlo no tenemos nestro empleado
+		//persistido
+		manager = emf.createEntityManager();
+		manager.getTransaction().begin();
+		//Ayuda a persistir una entidad y me devuelve la entidad persistida en el entity manager
+		e = manager.merge(e);
+		e.setNombre("Nombre modificado por segunda vez");
+		e.setApellidos("Apellido Modificado por segunda vez");
+		manager.getTransaction().commit();
+		imprimirTodo();
+		manager.close();
+		
+		//Elimina una entidad persistida
+		manager = emf.createEntityManager();
+		manager.getTransaction().begin();
+		e = manager.merge(e);
+		manager.remove(e);
+		manager.getTransaction().commit();
+		imprimirTodo();
+		manager.close();
+		
+		
 	}
 
 	private static void crearRegistro() {
+		EntityManager manager = emf.createEntityManager();
 		Empleado e = new Empleado(10L, "Perez", "Pepito", new GregorianCalendar(1979,6,6).getTime());
-		
 		manager.getTransaction().begin();
 		manager.persist(e);
 		manager.getTransaction().commit();
 	}
 	
 	private static void imprimirTodo() {
+		EntityManager manager = emf.createEntityManager();
 		List<Empleado> empleados = (List<Empleado>) manager.createQuery("FROM empleado").getResultList();
 		System.out.println("El número de empleados es de "+empleados.size());
 		for (Empleado emp:empleados) {
